344f0caecc0fdb08e2b05e871d92b0c6b06a736d
Nicolas Schejtman <nschejtman93@gmail.com>
2018-04-17 21:55:31 +0000

    Setup catkin workspace and created perceptual hash package

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /vagrant_data/catkin_ws/src
+# Build directory: /vagrant_data/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/vagrant_data/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/vagrant_data/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/vagrant_data/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/vagrant_data/catkin_ws/src:$ROS_PACKAGE_PATH"
