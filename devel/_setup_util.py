78e728fd571dac76733c251c4ad9ea4d7aecd194
Nicolas <nschejtman93@gmail.com>
2018-04-24 10:08:07 -0300

    Update devel files

--- devel/_setup_util.py
+++ devel/_setup_util.py
@@ -262,7 +262,7 @@ if __name__ == '__main__':
             sys.exit(1)
 
         # environment at generation time
-        CMAKE_PREFIX_PATH = '/opt/ros/lunar'.split(';')
+        CMAKE_PREFIX_PATH = '/vagrant_data/catkin_ws/devel;/opt/ros/lunar'.split(';')
         # prepend current workspace if not already part of CPP
         base_path = os.path.dirname(__file__)
         if base_path not in CMAKE_PREFIX_PATH:
