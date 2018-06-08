07f582c521444df8fdfd178cc765b000c9a84d3a
Nicolas Schejtman <nschejtman93@gmail.com>
2018-06-08 17:20:13 -0300

    Removed malfunctioning clock

--- src/chinese_paper/python/ChinesePaper.py
+++ src/chinese_paper/python/ChinesePaper.py
@@ -2,9 +2,8 @@ from InputParser import InputParser
 from DAVariant import DAVariant
 from BufferedFileReader import BufferedFileReader
 import cv2
-from ETAClock import ETAClock
 
-n_keypoints = 15  # 30
+n_keypoints = 30
 patch_size = 40
 
 reader = BufferedFileReader("/Users/nicolas/projects/deepLoopCloser/Dataset/dataset_1", ".ppm", 5)
@@ -13,13 +12,9 @@ da = DAVariant(n_keypoints=n_keypoints, patch_size=patch_size)
 
 n_batches = len(reader)
 
-clock = ETAClock(n_batches)
-clock.start()
 for i, batch in enumerate(reader):
     print("Started batch: " + str(i) + "/" + str(n_batches))
-    print("ETA: " + str(clock.get_eta()))
     cv_batch = list(map(lambda file: cv2.imread(file, cv2.IMREAD_GRAYSCALE), batch))
     parsed_batch = parser.calculate_all(cv_batch)
     da.fit(parsed_batch, warm_start=True)
-    clock.lap()
 
