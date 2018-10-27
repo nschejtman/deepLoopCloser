4eb0c293d631f8d7878c3b8901dcf57f2cb2dd26
Nicolas Schejtman <nschejtman93@gmail.com>
2018-10-27 11:02:04 -0300

    Added further logging

--- src/sdav/model/SDAV.py
+++ src/sdav/model/SDAV.py
@@ -13,18 +13,18 @@ class SDAV:
         self._configure_logging()
         logging.info('Initializing sdav')
 
+        self._set_train_path()
         self._define_model_and_training_params()
 
         self.losses = []
         self._sess = None
-
-        self._set_train_path()
 
         self._define_model_variables()
         self._define_fitting_model()
         self._define_optimizer()
         self._define_saver()
         self._define_summaries()
+        logging.info('Finished initializing sdav')
 
     def _define_model_and_training_params(self):
         self.input_shape = [30, 1681]
@@ -38,8 +38,22 @@ class SDAV:
         self.learning_rate: float = 0.1
         self.epochs: int = 100
         self.corruption_level: float = 0.3
+        logging.info('Parameter configuration: {\n' +
+                     '\tinput_shape: %s,\n' % self.input_shape +
+                     '\thidden_units: %s,\n' % self.hidden_units +
+                     '\tbatch_size: %s,\n' % self.batch_size +
+                     '\tcorruption_level: %s,\n' % self.corruption_level +
+                     '\tsparse_level: %s,\n' % self.sparse_level +
+                     '\tsparse_penalty: %s,\n' % self.sparse_penalty +
+                     '\tconsecutive_penalty: %s,\n' % self.consecutive_penalty +
+                     '\tbatch_size: %s,\n' % self.batch_size +
+                     '\tlearning_rate: %s,\n' % self.learning_rate +
+                     '\tepochs: %s,\n' % self.epochs +
+                     '\tcorruption_level: %s,\n' % self.corruption_level +
+                     '}')
 
     def _define_summaries(self):
+        logging.info('Defining summaries')
         tf.summary.histogram('w0-e', self._w0_e)
         tf.summary.histogram('b0-e', self._b0_e)
         tf.summary.histogram('w0-d', self._w0_d)
@@ -68,20 +82,24 @@ class SDAV:
         for i, loss in enumerate(self.losses):
             tf.summary.scalar('loss_%d' % i, loss)
 
-        self.log_dir = '%s/log' % self.train_path
         self._summary_op = tf.summary.merge_all()
-        self._summary_writer = tf.summary.FileWriter(self.log_dir, graph=tf.get_default_graph())
+        self._summary_writer = tf.summary.FileWriter(self.log_path, graph=tf.get_default_graph())
 
     def _set_train_path(self):
         root_path = firstParentWithNamePath(os.path.abspath(__file__), 'deepLoopCloser')
         self.train_path = root_path + '/training/sdav'
         Path(self.train_path).mkdir(parents=True, exist_ok=True)
-        Path(self.train_path + '/checkpoints').mkdir(parents=True, exist_ok=True)
-        Path(self.train_path + '/log').mkdir(parents=True, exist_ok=True)
+
+        self.checkpoints_path = self.train_path + '/checkpoints'
+        Path(self.checkpoints_path).mkdir(parents=True, exist_ok=True)
+
+        self.log_path = self.train_path + '/log'
+        Path(self.log_path).mkdir(parents=True, exist_ok=True)
 
         logging.info('Set training path to %s' % self.train_path)
 
     def _write_summaries(self, x_batch):
+        logging.info('Writing summaries')
         summary_str = self._sess.run(self._summary_op, feed_dict={self._x0: x_batch})
         self._summary_writer.add_summary(summary_str)
 
@@ -115,6 +133,7 @@ class SDAV:
         return tf.multiply(tf_zeros_mask, x, name=name) + tf_ones_mask
 
     def _define_fitting_model(self):
+        logging.info('Defining fitting model')
         batch_shape_0 = [self.batch_size, self.input_shape[0], self.input_shape[1]]
         flat_batch_shape_0 = [self.batch_size * self.input_shape[0], self.input_shape[1]]
 
@@ -159,22 +178,25 @@ class SDAV:
 
         self._define_loss(self._x4, self._h4, self._y4, 4)
 
-    def _define_loss(self, x, h, y, i):
-        cd_0 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=x, logits=y), name='cd_%d' % i)
+    def _define_loss(self, x, h, y, layer_n):
+        logging.info('Defining loss for layer %d' % layer_n)
+        cd_0 = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=x, logits=y), name='cd_%d' % layer_n)
 
-        cs_0 = tf.reduce_mean(tf.norm(h - self.sparse_level, axis=1, ord=1), name='cs_%d' % i)
+        cs_0 = tf.reduce_mean(tf.norm(h - self.sparse_level, axis=1, ord=1), name='cs_%d' % layer_n)
 
-        hidden_response_batch = tf.reshape(h, [self.batch_size, self.input_shape[0], self.hidden_units[i]])
+        hidden_response_batch = tf.reshape(h, [self.batch_size, self.input_shape[0], self.hidden_units[layer_n]])
         frames = tf.slice(hidden_response_batch, [0, 0, 0], [self.batch_size - 1, self.input_shape[0],
                                                              self.hidden_units[0]])
         frames_next = tf.slice(hidden_response_batch, [1, 0, 0],
-                               [self.batch_size - 1, self.input_shape[0], self.hidden_units[i]])
-        cc_0 = tf.reduce_mean(tf.norm(frames - frames_next, axis=[1, 2], ord='euclidean'), axis=0, name='cc_%d' % i)
+                               [self.batch_size - 1, self.input_shape[0], self.hidden_units[layer_n]])
+        cc_0 = tf.reduce_mean(tf.norm(frames - frames_next, axis=[1, 2], ord='euclidean'), axis=0,
+                              name='cc_%d' % layer_n)
 
         # Loss
         self.losses.append(cd_0 + self.sparse_penalty * cs_0 + self.consecutive_penalty * cc_0)
 
     def _define_model_variables(self):
+        logging.info('Defining model variables')
         self._w0_e = tf.Variable(
             tf.random_normal([self.input_shape[1], self.hidden_units[0]], dtype=tf.float64))
         self._b0_e = tf.Variable(tf.zeros([self.hidden_units[0]], dtype=tf.float64))
@@ -206,6 +228,7 @@ class SDAV:
         self._b4_d = tf.Variable(tf.zeros(self.hidden_units[3], dtype=tf.float64))
 
     def _create_dataset(self, file_pattern: str):
+        logging.info('Creating dataset')
         generator = get_generator(file_pattern, self.input_shape)
         return tf.data.Dataset.from_generator(generator, tf.float64)
 
@@ -214,19 +237,24 @@ class SDAV:
         return self.fit_dataset(dataset)
 
     def _define_optimizer(self):
+        logging.info('Defining optimizer')
         self.global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name='global_step')
         optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
         self.train_steps = list(map(lambda loss: optimizer.minimize(loss, global_step=self.global_step), self.losses))
 
     def _define_saver(self):
+        logging.info('Defining saver')
         self._saver = tf.train.Saver(save_relative_paths=True)
-        self.save_file = '%s/checkpoints/checkpoint_file' % self.train_path
+        self.checkpoint_file = '%s/checkpoint_file' % self.checkpoints_path
 
     def _load_or_init_session(self):
-        if len(os.listdir(self.train_path + '/checkpoints')) > 0:
-            self._saver.restore(self._sess, tf.train.latest_checkpoint(self.train_path + '/checkpoints'))
+        if len(os.listdir(self.checkpoints_path)) > 0:
+            latest_checkpoint = tf.train.latest_checkpoint(self.checkpoints_path)
+            logging.info('Restoring session from %s' % latest_checkpoint)
+            self._saver.restore(self._sess, latest_checkpoint)
         else:
             init_op = tf.global_variables_initializer()
+            logging.info('Initializing session')
             self._sess.run(init_op)
 
     def fit_dataset(self, dataset: tf.data.Dataset):
@@ -237,10 +265,9 @@ class SDAV:
                 self._sess.run(iterator.initializer)
                 logging.info('Fitting layer %d' % i)
                 batch_n = 0
+                self._load_or_init_session()
                 while True:
                     try:
-                        self._load_or_init_session()
-
                         batch = iterator.get_next()
                         stack_batch_op = tf.stack(batch)
                         stacked_batch = self._sess.run(stack_batch_op)
@@ -249,22 +276,19 @@ class SDAV:
                             break
 
                         for step in range(self.epochs):
-
                             self._sess.run(self.train_steps[i], feed_dict={self._x0: stacked_batch})
                             self._log_progress(batch_n, step, stacked_batch, i)
 
                         batch_n += 1
-
-                        logging.info('Writing summaries')
 
                         if (batch_n + 1) % 25 == 0:
                             self._write_summaries(stacked_batch)
 
                     except tf.errors.OutOfRangeError:
                         break
-                        
-                logging.info('Saving trained params')
-                self._saver.save(self._sess, self.save_file, global_step=self.global_step)
+
+                logging.info('Saving trained params to %s with global_step %s' % (self.checkpoint_file, self.global_step))
+                self._saver.save(self._sess, self.checkpoint_file, global_step=self.global_step)
 
     def _log_progress(self, batch_n, step, x_batch, layer_n):
         progress_str = '    Layer:%d Batch:%d fit, Epoch:%d/%d, Loss:%s'
