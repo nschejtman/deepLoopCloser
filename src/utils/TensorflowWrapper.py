fe020d63baeeb9f1531cfbcfd2dff8c9c1e55c48
Nicolas Schejtman <nschejtman93@gmail.com>
2019-01-03 00:57:31 -0300

    Add new methods, protocol implementations and native operations to TensorWrapper

--- src/utils/TensorflowWrapper.py
+++ src/utils/TensorflowWrapper.py
@@ -10,25 +10,47 @@ class TensorWrapper:
         return self.reshape([shape[0] * shape[1], shape[2]])
 
     def batch(self, batch_size):
+        batch_size = parameter_guard(batch_size)
         shape = self.shape()
         return self.reshape([batch_size, tf.cast(shape[0] / batch_size, tf.int32), shape[1]])
 
     def batch_size(self):
-        return self.shape()[0]
+        if self.dimensions() == 3:  # TODO replace with rank
+            return self.shape()[0]
+        else:
+            return 1
 
-    def corrupt(self, mask):
-        return TensorWrapper(tf.multiply(self.x, mask))
+    def parameter_number(self):
+        if self.dimensions() == 3:
+            return self.shape()[1] * self.shape()[2]
+        else:
+            return self.shape()[0] * self.shape()[1]
+
+    def corrupt(self, corruption_level):
+        shape = self.shape()
+        shape = shape[1:] if self.dimensions() == 3 else shape
+        mask = random_mask(shape, corruption_level)
+        return self.multiply(mask)
+
+    def rank(self):
+        return TensorWrapper(tf.rank(self.x))
 
     def dimensions(self):
         return len(self.x.get_shape())
 
     def shape(self):
-        return tf.shape(self.x)
+        return TensorWrapper(tf.shape(self.x))
+
+    def concat(self, y, axis=0):
+        y = parameter_guard(y)
+        return TensorWrapper(tf.concat([self.x, y], axis))
 
     def reshape(self, shape):
+        shape = parameter_guard(shape)
         return TensorWrapper(tf.reshape(self.x, shape))
 
     def matmul(self, y):
+        y = parameter_guard(y)
         yw = TensorWrapper(y)
         y_dims = yw.dimensions()
         x_dims = self.dimensions()
@@ -40,18 +62,80 @@ class TensorWrapper:
             return self.flat_batch().matmul(y).batch(batch_size)
 
     def add(self, y):
+        y = parameter_guard(y)
         return TensorWrapper(self.x + y)
+
+    def multiply(self, y):
+        y = parameter_guard(y)
+        return TensorWrapper(tf.multiply(self.x, y))
 
     def sigmoid(self):
         return TensorWrapper(tf.nn.sigmoid(self.x))
+
+    def shuffle(self):
+        return TensorWrapper(tf.random.shuffle(self.x))
+
+    def __getitem__(self, item):
+        item = parameter_guard(item)
+        return TensorWrapper(self.x[item])
+
+    def __mul__(self, y):
+        y = parameter_guard(y)
+        return TensorWrapper(self.x * y)
+
+    def __rmul__(self, y):
+        y = parameter_guard(y)
+        return TensorWrapper(y * self.x)
+
+
+    def __add__(self, y):
+        y = parameter_guard(y)
+        return TensorWrapper(self.x + y)
+
+    def __sub__(self, y):
+        y = parameter_guard(y)
+        return TensorWrapper(self.x - y)
 
     def to_tf(self):
         return self.x
 
 
+def parameter_guard(y):
+    if isinstance(y, TensorWrapper):
+        return y.to_tf()
+    else:
+        return y
+
+
 def placeholder(dtype, shape):
+    shape = parameter_guard(shape)
     return TensorWrapper(tf.placeholder(dtype, shape=shape))
 
 
-def constant(value, dtype=tf.float64):
-    return TensorWrapper(tf.constant(value, dtype=dtype))
+def constant(value, shape=None, dtype=tf.float64):
+    if shape is None:
+        shape = []
+    else:
+        shape = parameter_guard(shape)
+    return TensorWrapper(tf.constant(value, shape=shape, dtype=dtype))
+
+
+def zeros(shape, dtype=tf.float64):
+    shape = parameter_guard(shape)
+    return TensorWrapper(tf.zeros(shape, dtype=dtype))
+
+
+def ones(shape, dtype=tf.float64):
+    shape = parameter_guard(shape)
+    return TensorWrapper(tf.ones(shape, dtype=dtype))
+
+
+def random_mask(shape, zeros_percentage, dtype=tf.float64):
+    shape = parameter_guard(shape)
+    zeros_percentage = parameter_guard(zeros_percentage)
+
+    parameters = shape[0] * shape[1]
+    n_zeros = parameters * zeros_percentage
+    n_ones = parameters - n_zeros
+
+    return ones(n_ones, dtype=dtype).concat(zeros(n_zeros, dtype=dtype)).shuffle()
